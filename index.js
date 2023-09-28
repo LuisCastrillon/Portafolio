c45a4a5f4f77498dac5045d5d12f014ce3034a1c
Luis Castrillon <tutukastrillon@gmail.com>
2023-09-27 23:19:05 -0500

    corregi algunos errores

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const cntImg = document.querySelector(".cntImg")
 const main2 = document.querySelector(".main2")
 const about = document.querySelector(".about")
+const inicio = document.querySelector(".inicio")
+const linkFooter = document.querySelector(".linkFooter")
 
-cntImg.addEventListener("click", () => {
-    main2.scrollIntoView({ behavior: "smooth" });
-});
+
+if(cntImg){
+    cntImg.addEventListener("click", () => {
+        main2.scrollIntoView({ behavior: "smooth" })
+    })
+}
 
 about.addEventListener("click", () => {
     window.location.href = "about.html"
-})
+});
 
+inicio.addEventListener("click", () => {
+    window.location.href = "index.html";
+});
 
-
-console.log(main2)
-console.log(about)
