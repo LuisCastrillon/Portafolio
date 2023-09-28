b89b4d895065e147793653ac4c04e0fd1e876756
Luis Castrillon <tutukastrillon@gmail.com>
2023-09-27 20:50:18 -0500

    Termine la pagina que contiene mi informacion

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const cntImg = document.querySelector(".cntImg")
 const main2 = document.querySelector(".main2")
+const about = document.querySelector(".about")
 
 cntImg.addEventListener("click", () => {
     main2.scrollIntoView({ behavior: "smooth" });
+});
+
+about.addEventListener("click", () => {
+    window.location.href = "about.html"
 })
+
+
+
+console.log(main2)
+console.log(about)
