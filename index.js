028fb698201b79b977ccfe63754d63e7ce5e538e
Luis Castrillon <tutukastrillon@gmail.com>
2023-09-28 23:59:55 -0500

    pagina para version desktop terminada

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 const cntImg = document.querySelector(".cntImg")
-const main2 = document.querySelector(".main2")
-const about = document.querySelector(".about")
-const inicio = document.querySelector(".inicio")
+const workSeccion = document.querySelector(".main2")
+const aboutDiv = document.querySelector(".about")
+const workDiv = document.querySelector(".inicio")
 const linkFooter = document.querySelector(".linkFooter")
+const toInicio = document.querySelector(".toInicio")
+// const correoFooterDiv = document.querySelector("#correoFooterDiv")
+// const linkdinFooterDiv = document.querySelector("#linkdinFooterDiv")
+// const curriculumFooterDiv = document.querySelector("#curriculumFooterDiv")
+// const instagramFooterDiv = document.querySelector("#workFooterDiv")
 
 
 if(cntImg){
-    cntImg.addEventListener("click", () => {
-        main2.scrollIntoView({ behavior: "smooth" })
-    })
+    cntImg.addEventListener("click", workPage)
 }
 
-about.addEventListener("click", () => {
-    window.location.href = "about.html"
+// linkdinFooterDiv.addEventListener("click", linkdin)
+// curriculumFooterDiv.addEventListener("click", curriculum)
+// instagramFooterDiv.addEventListener("click", ig)
+// correoFooterDiv.addEventListener("click", correo)
+toInicio.addEventListener("click", inicioPage)
+aboutDiv.addEventListener("click", aboutPage);
+workDiv.addEventListener("click", () => {
+    if(workSeccion){
+        workPage()
+    }else{
+        inicioPage()
+    }
 });
 
-inicio.addEventListener("click", () => {
+
+
+function inicioPage (){
     window.location.href = "index.html";
-});
+}
 
+function workPage(){
+    workSeccion.scrollIntoView({ behavior: "smooth" })
+}
+
+function aboutPage (){
+    window.location.href = "about.html"
+}
+
+// function correo(){
+//     window.location.href = "mailto:tutukastrillon@gmail.com"
+// }
